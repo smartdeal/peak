1870e2b893e918f3c791f1760c3eb65206240779
smartdeal <smartdealua@gmail.com>
2017-05-22 11:39:28 +0300

    bug fix change page-catalog change page name store-locator

--- page-product.php
+++ page-product.php
@@ -18,6 +18,7 @@
     <section class="page__content">
         <div class="container">
             <?php if (have_posts()) {while (have_posts()) { the_post(); ?>        
+            <div class="product">
                 <div class="product__imgwr">
                     <div class="product__img">
                         <?php if (has_post_thumbnail()) echo '<img class="content__img" src="'.wp_get_attachment_image_url(get_post_thumbnail_id(),'large').'">'; ?>
@@ -28,6 +29,7 @@
                     </div>
                     <div class="product__slogan">Все об антифризах. Профессионально.</div>
                 </div>
+                <div class="product__body">
                     <div class="product__title">
                         <h1 itemprop="headline"><?php the_title(); ?></h1>
                     </div>    
@@ -49,52 +51,54 @@
                                 <?php } ?>
                             <?php } ?>
                     </div>
-                <div class="product__desc">
-                    <div class="product__sects">
-                        <div id="product-desc" class="product__sect">
-                            <div class="product__sect-desc" itemprop="articleBody">
-                                <?php the_content(); ?>
+                    <div class="product__desc">
+                        <div class="product__sects">
+                            <div id="product-desc" class="product__sect">
+                                <div class="product__sect-desc" itemprop="articleBody">
+                                    <?php the_content(); ?>
+                                </div>
+                                <div class="product__btnwr">
+                                    <a class="btn btn_yellow btn_pr-buy" href="/where-buy">Купить</a>
+                                    <a class="btn btn_yellow btn_pr-part" href="/contacts">Стать партнером</a>
+                                </div>
                             </div>
-                            <div class="product__btnwr">
-                                <a class="btn btn_yellow btn_pr-buy" href="/where-buy">Купить</a>
-                                <a class="btn btn_yellow btn_pr-part" href="/contacts">Стать партнером</a>
-                            </div>
+                            <?php if ($product_allow = get_field('allow')) { ?>
+                                <div id="product-allow" class="product__sect">
+                                    <div class="product__sect-title">Допуски и соответствия</div>
+                                    <div class="product__sect-desc product__sect-desc_allow">
+                                        <?php echo $product_allow; ?>
+                                    </div>
+                                </div>
+                            <?php } ?>
+                            <?php if ($product_use = get_field('use')) { ?>
+                                <div id="product-use" class="product__sect">
+                                    <div class="product__sect-title">Применение</div>
+                                    <div class="product__sect-desc">
+                                        <?php echo $product_use; ?>
+                                    </div>
+                                </div>
+                            <?php } ?>
+                            <?php if ($product_adv = get_field('adv')) { ?>
+                                <div id="product-adv" class="product__sect">
+                                    <div class="product__sect-title">Преимущества</div>
+                                    <div class="product__sect-desc">
+                                        <ul class="product__adv-items">
+                                        <?php 
+                                            foreach ($product_adv as $k) {
+                                                echo '<li class="product__adv-item">';
+                                                echo '<div class="product__adv-title">'.$k['title'].'</div>';
+                                                echo '<div class="product__adv-txt">'.$k['txt'].'</div>';
+                                                echo '</li>';
+                                            }
+                                        ?>
+                                        </ul>
+                                    </div>
+                                </div>
+                            <?php } ?>
                         </div>
-                        <?php if ($product_allow = get_field('allow')) { ?>
-                            <div id="product-allow" class="product__sect">
-                                <div class="product__sect-title">Допуски и соответствия</div>
-                                <div class="product__sect-desc product__sect-desc_allow">
-                                    <?php echo $product_allow; ?>
-                                </div>
-                            </div>
-                        <?php } ?>
-                        <?php if ($product_use = get_field('use')) { ?>
-                            <div id="product-use" class="product__sect">
-                                <div class="product__sect-title">Применение</div>
-                                <div class="product__sect-desc">
-                                    <?php echo $product_use; ?>
-                                </div>
-                            </div>
-                        <?php } ?>
-                        <?php if ($product_adv = get_field('adv')) { ?>
-                            <div id="product-adv" class="product__sect">
-                                <div class="product__sect-title">Преимущества</div>
-                                <div class="product__sect-desc">
-                                    <ul class="product__adv-items">
-                                    <?php 
-                                        foreach ($product_adv as $k) {
-                                            echo '<li class="product__adv-item">';
-                                            echo '<div class="product__adv-title">'.$k['title'].'</div>';
-                                            echo '<div class="product__adv-txt">'.$k['txt'].'</div>';
-                                            echo '</li>';
-                                        }
-                                    ?>
-                                    </ul>
-                                </div>
-                            </div>
-                        <?php } ?>
                     </div>
                 </div>
+            </div>
             <?php }} ?>
         </div>
     </section>
